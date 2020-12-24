7718895a49b45e96748de69e296a467b268bc6ce
Charles Liu <liu.charles02@gmail.com>
2020-12-24 01:20:24 -0800

    added canvas, clears after resizing

--- client/src/client.js
+++ client/src/client.js
@@ -1,3 +1,4 @@
+//Updates chat
 const log = (text) => {
     const parent = document.querySelector('#events')
     const elem = document.createElement('li')
@@ -14,17 +15,66 @@ const onChatSubmitted = (socket) => (e) => {
     socket.emit('message', text)
 }
 
+var pos = {x: 0, y: 0}
+
+const updateBoard = (canvas) => {
+    const ctx = canvas.getContext('2d')
+
+    //New position from mouse event
+    const setPosition = (e) => {
+        const {top, left} = canvas.getBoundingClientRect()
+        const {clientX, clientY} = e
+        pos.x = clientX - left
+        pos.y = clientY - top
+    }
+
+    const draw = (e) => {
+        //Checks that mouse left button is pressed
+        if (e.buttons !== 1) {
+            return
+        }
+        ctx.beginPath();
+
+        ctx.lineWidth = 2
+        ctx.linecap = 'round'
+        ctx.strokeStyle = 'black'
+        
+        //From
+        ctx.moveTo(pos.x, pos.y)
+        setPosition(e)
+        //To
+        ctx.lineTo(pos.x, pos.y)
+        //Draws line
+        ctx.stroke()
+    }
+
+    return {setPosition, draw}
+}
+
 (() => {
     const socket = io()
     const canvas = document.querySelector('canvas')
+    const {width} = canvas.getBoundingClientRect()
+    canvas.width = width
+    const heightRatio = 0.67
+    canvas.height = canvas.width * heightRatio
 
-    const onClick = (e) => {
-        const {x, y} = getClickCoordinates(canvas, e)
+    const resizeCanvas = (e) => {
+        const {width} = canvas.getBoundingClientRect()
+        canvas.width = width
+        const heightRatio = 0.67
+        canvas.height = canvas.width * heightRatio
     }
 
     socket.on('message', log)
 
     document.querySelector('#chat-form').addEventListener('submit', onChatSubmitted(socket))
 
-    canvas.addEventListener('click', onClick)
+    const {setPosition} = updateBoard(canvas)
+    const {draw} = updateBoard(canvas)
+
+    window.addEventListener('resize', resizeCanvas)
+    document.addEventListener('mousemove', draw)
+    document.addEventListener('mousedown', setPosition)
+    document.addEventListener('mouseenter', setPosition)
 })();
