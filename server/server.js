03592153f55e1b32266510b0703974a4da8e897c
Charles Liu <liu.charles02@gmail.com>
2021-01-05 11:17:14 -0800

    show votes

--- server/server.js
+++ server/server.js
@@ -226,7 +226,7 @@ io.on('connection', (socket) => {
         if (getRoomUsers(getCurrentUser(socket.id).room).length === numUsersVoted) {
             // Change votes map to JSON to pass as socket io parameter
             let votesJson = JSON.stringify(Array.from(votes));
-            // io.to(room).emit('show votes', votesJson)
+            io.to(room).emit('show votes', votesJson)
             const maxVotes = Math.max(...votes.values())
             // If Fake Artist has the max amount of votes, they lose
             if (roomToVotes.get(room).get(roomToFakeArtist.get(room)) === maxVotes) {
